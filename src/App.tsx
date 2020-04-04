4e6f116c53873079100465bba42007591bb25e40
Vadim Bondarenko <vadim.b@develops.today>
2020-04-04 17:01:03 +0300

    feat(app): add mui and implement basic accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary, Container, Typography} from '@material-ui/core';
+import { ExpandMore } from '@material-ui/icons';
 
-function App() {
+enum ExpansionPanelEnum {
+  Overview = 'Overview',
+  TechnicalRequirement = 'TechnicalRequirement',
+  Journal = 'Journal',
+  Additional = 'Additional',
+}
+
+
+const App = () => {
+  const [expanded, setExpanded] = useState('');
+
+  const handleChange = (panel: string) => setExpanded(panel)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <ExpansionPanel expanded={expanded === ExpansionPanelEnum.Overview} onChange={() => handleChange(ExpansionPanelEnum.Overview)}>
+        <ExpansionPanelSummary expandIcon={<ExpandMore />}>
+          <Typography>General settings</Typography>
+          <Typography>I am an expansion panel</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
+            maximus est, id dignissim quam.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel expanded={expanded === ExpansionPanelEnum.TechnicalRequirement} onChange={() => handleChange(ExpansionPanelEnum.TechnicalRequirement)}>
+        <ExpansionPanelSummary expandIcon={<ExpandMore />}>
+          <Typography>General settings</Typography>
+          <Typography>I am an expansion panel</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel expanded={expanded === ExpansionPanelEnum.Journal} onChange={() => handleChange(ExpansionPanelEnum.Journal)}>
+        <ExpansionPanelSummary expandIcon={<ExpandMore />}>
+          <Typography>General settings</Typography>
+          <Typography>I am an expansion panel</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
+            maximus est, id dignissim quam.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel expanded={expanded === ExpansionPanelEnum.Additional} onChange={() => handleChange(ExpansionPanelEnum.Additional)}>
+        <ExpansionPanelSummary expandIcon={<ExpandMore />}>
+          <Typography>General settings</Typography>
+          <Typography>I am an expansion panel</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
+            maximus est, id dignissim quam.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </Container>
   );
 }
 
