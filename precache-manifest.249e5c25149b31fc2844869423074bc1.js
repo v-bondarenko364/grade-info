78ac08e0f9a83d34f5733941b65a272e259618ea
Vadim Bondarenko <vadim.b@develops.today>
2020-04-04 19:48:26 +0300

    Updates

---
+++ precache-manifest.249e5c25149b31fc2844869423074bc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a7f8f5bbd677ce17ffb56b79ade754",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22fcef0c50ea44e31c68",
+    "url": "/static/css/main.e280a983.chunk.css"
+  },
+  {
+    "revision": "c30855ab27b956e6bdce",
+    "url": "/static/js/2.f2c8148d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f2c8148d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22fcef0c50ea44e31c68",
+    "url": "/static/js/main.09f59874.chunk.js"
+  },
+  {
+    "revision": "8e27397c75a235a1e007",
+    "url": "/static/js/runtime-main.36c5f8c9.js"
+  },
+  {
+    "revision": "dd303e525c150c3d399c58c494a9e843",
+    "url": "/static/media/bg.dd303e52.jpg"
+  }
+]);
